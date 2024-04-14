66f618dc69ed643a9e645c78af51eeb869dbf2f6
MurielDelvaux <muriel@keymax.com.br>
2024-04-14 15:23:03 -0300

    first commit

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+            <p>Conteúdo da página inicial...</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*tsx'
+  ],
+  theme: {
+    dark:{
+      background:"#0F0F11"
+    },
+    light:{
+      background:"#BEBEFF"
+    }
+  },
+  plugins: [],
+}
+
