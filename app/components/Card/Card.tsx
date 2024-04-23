f0d45e745d68452e2f417dfdd3974f9b69a385c2
Muriel Delvaux Daher <muriel.daher@trinusco.com.br>
2024-04-23 19:38:16 -0300

    fix: adjust layout

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -4,9 +4,9 @@ import clsx from "clsx";
 
 export const Card = () => {
     return (
-        <div className={clsx("flex flex-col w-72 h-[28rem] border-solid border-2 rounded border-light-purple bg-light-card ")}>
-            <div>imagem</div>
-            <div className={clsx("flex flex-row w-full justify-between")}>
+        <div className={clsx("flex flex-col justify-center items-center gap-y-6 w-72 h-[28rem] border-solid border-2 rounded border-light-purple bg-light-card")}>
+            <div className={clsx("flex w-64 h-80 border-solid border-1 border-light-purple")}>imagem</div>
+            <div className={clsx("flex flex-row w-64 justify-between")}>
                 <div className={clsx("flex flex-col")}>
                     <div>item</div>
                     <div>item</div>
